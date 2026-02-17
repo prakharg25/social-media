d804e44c24d136f126d318bd4af710dad29727a9
prakharg25 <157348260+prakharg25@users.noreply.github.com>
2026-02-17 20:24:51 +0530

    Update API base URL for authentication requests

--- frontend/src/Api/AuthRequest.js
+++ frontend/src/Api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://socialmedia-backend-67gp.onrender.com" });
+const API = axios.create({ baseURL: "https://social-media-backend-r8fg.onrender.com" });
 
 export const logIn = async (formData) =>await API.post('/auth/login',formData);
 
