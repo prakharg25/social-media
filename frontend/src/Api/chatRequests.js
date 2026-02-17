4fdcd37d4bc5ee702e81a2e09a5abd57bb526ae8
prakharg25 <157348260+prakharg25@users.noreply.github.com>
2026-02-17 20:40:58 +0530

    Update chatRequests.js

--- frontend/src/Api/chatRequests.js
+++ frontend/src/Api/chatRequests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-const API=axios.create({baseURL:"https://social-media-backend-r8fg.onrender.com"});
+const API=axios.create({baseURL:"https://social-media-1-ff39.onrender.com"});
 
 export const userChats=(userId,token)=>API.get(`/chat/${userId}`, {
     headers: {
